b2fa3d24f457690603a3475409432e34623705ae
pi66 <pixel2176@proton.me>
2026-05-01 11:58:49 +0100

    fix: bugs on sta

--- sta/config.def.h
+++ sta/config.def.h
@@ -8,14 +8,14 @@
 static const Args commands[] = {
 	// format     command      id             delay
 	{run_command  , " %s "     , "date +%H:%M", 1, 60*1000},
-	{run_command  , " %s                              ", "date +'%b %d'", 2, 60*1000},
+	{run_command  , " %s                                          ", "date +'%b %d'", 2, 60*1000},
 	{battery_state, " %s "     , "BAT1", 3, 1000},
 	{battery_perc , "   %s% │", "BAT1", 4, 60*2000},
-	{run_command  , "   %s │", "brightnessctl | grep Current | awk '{print $4}' | tr -d '()'", 5, 0},
+	{run_command  , "   %s │" , "brightnessctl | grep Current | awk '{print $4}' | tr -d '()'", 5, 0},
 	{run_command  , "   %s │" , "pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | head -n1", 6, 0},
-	{cpu_perc     , "  %s% /" , NULL, 7, 1000},
-	{temp         , "  %s󰔄 │" , "/sys/class/thermal/thermal_zone3/temp", 8, 1000},
 	{ram_used     , "   %s │" , NULL, 9, 1000},
 	{disk_perc    , "   %s% │", "/home/", 10, 60*1000},
 	{wifi_essid   , "   %s │" , "wlan0", 11, 60*1000},
+	{run_command  , "   %s  " , "python /home/pixel/.src/scripts/server.py", 12, 1000},
+
 };
